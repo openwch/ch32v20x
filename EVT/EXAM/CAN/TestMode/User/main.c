eec5b060195598bdaae7f6bcc4a502a34a6b3be3
qiyongshuang <1210052066@qq.com>
2024-05-09 16:45:21 +0800

    v2.1

--- EVT/EXAM/CAN/TestMode/User/main.c
+++ EVT/EXAM/CAN/TestMode/User/main.c
@@ -2,7 +2,7 @@
  * File Name          : main.c
  * Author             : WCH
  * Version            : V1.0.0
- * Date               : 2021/06/06
+ * Date               : 2024/04/06
  * Description        : Main program body.
  *********************************************************************************
  * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
@@ -234,7 +234,7 @@ int main(void)
             printf("Receive Data:\r\n");
 
             for(i = 0; i < 8; i++) {
-                printf("%02x\r\n", txbuf[i]);
+                printf("%02x\r\n", rxbuf[i]);
             }
         }
         else
